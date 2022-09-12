5e952912e38536ada0aae12cb2ac2e053ab2349a
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-12 22:50:59 +0300

    add validate name function

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,31 @@ window.onload = () => {
   const closeWindow = document.getElementById("close-window");
   const popupFirst = document.getElementById("popup-first");
   const linksHeader = document.querySelectorAll("header a");
-  
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let number = document.getElementById("number");
+  let message = document.getElementById("message");
+  let validateSection = document.getElementById("contact-validation");
+  let validateText = document.getElementById("contact-validation-text");
+  let contactSubmit = document.getElementById("contact-submit");
+  let probName;
+  let probEmail;
+  let probNumber;
+  let probMessage;
 
   // Functions
+
+  // open popup
   let openPopup = () => {
     popupFirst.classList.add("popup-visible");
   };
+
+  // close popup
   let closePopup = () => {
     popupFirst.classList.remove("popup-visible");
   };
+
+  // make header smaller when scrolling dwown
   let scroll = () => {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       linksHeader.forEach(l => {
@@ -24,11 +40,20 @@ window.onload = () => {
       });
     }
   };
+
+  //  validate name
+  let checkName = () => {
+    if (name.value.length < 5) {
+      probName = "Please enter your full name (Name should be at least 5 characters long)";
+      return false;
+    }
+    return true;
+  };
+
   //
   window.onscroll = () => {
     scroll();
   };
-
   portfolioFirst.addEventListener("click", openPopup);
   closeWindow.addEventListener("click", closePopup);
 };
