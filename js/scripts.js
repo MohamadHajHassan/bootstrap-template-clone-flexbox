cb8bb79b77023cbe0120ac4bb3b7104a9bc5bbb3
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-12 21:38:35 +0300

    add small header feature when scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ window.onload = () => {
   const portfolioFirst = document.getElementById("portfolio-first");
   const closeWindow = document.getElementById("close-window");
   const popupFirst = document.getElementById("popup-first");
+  const linksHeader = document.querySelectorAll("header a");
+  
 
   // Functions
   let openPopup = () => {
@@ -11,8 +13,22 @@ window.onload = () => {
   let closePopup = () => {
     popupFirst.classList.remove("popup-visible");
   };
-
+  let scroll = () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      linksHeader.forEach(l => {
+        l.classList.add("small-header");
+      });
+    } else {
+      linksHeader.forEach(l => {
+        l.classList.remove("small-header");
+      });
+    }
+  };
   //
+  window.onscroll = () => {
+    scroll();
+  };
+
   portfolioFirst.addEventListener("click", openPopup);
   closeWindow.addEventListener("click", closePopup);
 };
